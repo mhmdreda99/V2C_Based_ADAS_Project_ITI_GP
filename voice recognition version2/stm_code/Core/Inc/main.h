c3ab7091d8f64257e5b4aa5b59479816a9bafaa6
abdel-hamidd <abdelhamidtarek62@gmail.com>
2023-10-12 08:44:04 +0300

    #MODIFIED: voice recognition V2.0

---
+++ voice recognition version2/stm_code/Core/Inc/main.h
@@ -0,0 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+#define MOTOR_PORT     GPIOA
+#define MOTOR_PIN      GPIO_PIN_1
+#define AC_PORT        GPIOA
+#define AC_PIN         GPIO_PIN_0
+
+#define ACTIVE_MOTOR   GPIO_PIN_SET
+#define DISABLE_MOTOR  GPIO_PIN_RESET
+
+#define ACTIVE_AC      GPIO_PIN_SET
+#define DISABLE_AC     GPIO_PIN_RESET
+
+#define OPEN_SYSTEM     5
+#define CLOSE_SYSTEM    6
+#define OPEN_AC         4
+#define CLOSE_AC        1
+#define OPEN_MOTOR      2
+#define CLOSE_MOTOR     3
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
