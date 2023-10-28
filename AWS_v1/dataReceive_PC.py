1f8e0aec8feabaa6e2f8bdd8a161c32d57086ace
Mostafa Ibrahim <eng.mostafaibrahmim97@gmail.com>
2023-10-28 17:54:27 +0200

    #Code Modified

--- AWS_v1/dataReceive_PC.py
+++ AWS_v1/dataReceive_PC.py
@@ -11,10 +11,10 @@ def DispTime(self,params,packet):
 		print("\n")
 		
 AWShost = "a38ng4g7lgwyr7-ats.iot.eu-north-1.amazonaws.com"
-AWSport = 8883	
-rootCaPath = "/home/abdelhamid/Desktop/AWS_GP/AmazonRootCA1.pem"
-certPath = "/home/abdelhamid/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-private.pem.key"
-keyPath = "/home/abdelhamid/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-certificate.pem.crt"	
+AWSport = 8883
+rootCaPath = "/home/mostafa/Desktop/AWS_GP/AmazonRootCA1.pem"
+certPath = "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-private.pem.key"
+keyPath = "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-certificate.pem.crt"	
 		
 myMQTTClient = AWSIoTMQTTClient("ItiGpIDSubscriber") #Creating an Object 
 myMQTTClient.configureEndpoint(AWShost, AWSport)#Providing End point from AWS URL
@@ -29,7 +29,7 @@ myMQTTClient.connect() #connect to the broker
 
 
 while True:
-	myMQTTClient.subscribe("RealTimeDataTrasfer/Temperature",1,DispTemp)
-	myMQTTClient.subscribe("RealTimeDataTrasfer/Fuellevel",1,DispFuelLevel)
-	myMQTTClient.subscribe("RealTimeDataTrasfer/Time",1,DispTime)
+	myMQTTClient.subscribe("RealTimeDataTransfer/Temperature",1,DispTemp)
+	myMQTTClient.subscribe("RealTimeDataTransfer/Fuellevel",1,DispFuelLevel)
+	myMQTTClient.subscribe("RealTimeDataTransfer/Time",1,DispTime)
 	time.sleep(5)
--- AWS_v1/datatransfer_PC.py
+++ AWS_v1/datatransfer_PC.py
@@ -2,25 +2,44 @@ from random import randrange,uniform
 import time
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
+AWShost = "a38ng4g7lgwyr7-ats.iot.eu-north-1.amazonaws.com"
+AWSport = 8883
+rootCaPath = "/home/mostafa/Desktop/AWS_GP/AmazonRootCA1.pem"
+certPath = "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-private.pem.key"
+keyPath = "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-certificate.pem.crt"
+
+
 myMQTTClient = AWSIoTMQTTClient("ItiGpIDPublisher")
-myMQTTClient.configureEndpoint("a38ng4g7lgwyr7-ats.iot.eu-north-1.amazonaws.com", 8883)
-
-myMQTTClient.configureCredentials("/home/mostafa/Desktop/AWS_GP/AmazonRootCA1.pem", "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-private.pem.key", "/home/mostafa/Desktop/AWS_GP/e1d57696d99123be86537cabd0ceb0793150662e77ad1a7796822f3652a9ebda-certificate.pem.crt")
-
+myMQTTClient.configureEndpoint(AWShost, AWSport)
+myMQTTClient.configureCredentials(rootCaPath,certPath, keyPath)
 myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5) # 5 sec
-print ('Initiating Realtime Data Transfer From PC...')
+
+print ('Initiating Realtime Data Transfer From Gateway Raspberry Pi...')
 myMQTTClient.connect()
 
 
 while True:
-    randNumber = uniform(20,26)
-    print("publish from Rasp.."+str(randNumber))
+    Temp = int(uniform(20,26))
+    print("publish from Rasp.."+str(Temp))
     myMQTTClient.publish(
        topic="RealTimeDataTransfer/Temperature",
        QoS=1,
-       payload='{"Temperature":"'+str(randNumber)+'"}')
+       payload='{"Temperature:"'+str(Temp)+'"}')
+    FuelLevel = int(uniform(50,60))
+    print("publish from Rasp.."+str(FuelLevel))
+    myMQTTClient.publish(
+       topic="RealTimeDataTransfer/Fuellevel",
+       QoS=1,
+       payload='{"Fuel Level:"'+str(FuelLevel)+'"}')
+    Time = int(uniform(500,1000))
+    print("publish from Rasp.."+str(Time))
+    myMQTTClient.publish(
+       topic="RealTimeDataTransfer/Time",
+       QoS=1,
+       payload='{"Elapsed Time:"'+str(Time)+'"}')   
     time.sleep(2)
+
 
